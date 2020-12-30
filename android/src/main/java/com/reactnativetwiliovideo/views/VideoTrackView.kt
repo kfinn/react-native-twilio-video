1cef993545ba286976b543aa94a3e695bd26714c
Kevin Finn <superkevin@gmail.com>
2020-12-30 11:07:38 -0500

    fix: tweak android behavior, fix a couple bugs

--- android/src/main/java/com/reactnativetwiliovideo/views/VideoTrackView.kt
+++ android/src/main/java/com/reactnativetwiliovideo/views/VideoTrackView.kt
@@ -5,6 +5,7 @@ import android.widget.LinearLayout
 import com.twilio.video.VideoScaleType
 import com.twilio.video.VideoTrack
 import com.twilio.video.VideoView
+import java.util.logging.Logger
 
 class VideoTrackView(context: Context): LinearLayout(context) {
   private val videoView = VideoView(context)
@@ -15,10 +16,15 @@ class VideoTrackView(context: Context): LinearLayout(context) {
 
   var videoTrack: VideoTrack? = null
     set(value) {
-      if (value != field) {
-        field?.removeSink(videoView)
+      val oldValue = field
+
+      if (oldValue != value) {
+        if (oldValue != null && oldValue.sinks.contains(videoView)) {
+          oldValue.removeSink(videoView)
+        }
         value?.addSink(videoView)
       }
+
       field = value
     }
 
